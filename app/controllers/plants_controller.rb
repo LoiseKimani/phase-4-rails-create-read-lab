b9544fb4ad31d8aeb3ae17de87f3acca5f546267
Loise <loisewakonyo18@gmail.com>
2023-03-20 11:15:02 +0300

    Done

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,61 +1,19 @@
-# class PlantController < ApplicationController
-# end
-
-# class PlantsController < ApplicationController
-
-#   def index
-#     plants = Plant.all
-#     render json: plants
-#   end
-  
-#   def show
-#     plant = Plant.find(params[:id])
-#     render json: plant
-#   end
-  
-#   def create
-#        plant = Plant.new(plant_params)
-#   if plant.save
-#       render json: plant, status: :created
-#   else
-#        render json: { error: plant.errors.full_messages }, status: :unprocessable_entity
-#   end
-#   end
-  
-#
-  
 class PlantsController < ApplicationController
-  before_action :set_plant, only: [:show]
-
   def index
-    plants = Plant.all
-    render json: plants, status: :ok
+    render json: Plant.all
   end
 
   def show
-    render json: @plant, status: :ok
+    render json: Plant.find(params[:id])
   end
 
   def create
-    plant = Plant.new(plant_params)
-
-    if plant.save
-      render json: plant, status: :created
-    else
-      render json: { error: plant.errors.full_messages }, status: :unprocessable_entity
-    end
+    render json: Plant.create(plant_params), status: :created
   end
 
   private
 
-  def set_plant
-    @plant = Plant.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    render json: { error: "Plant with ID #{params[:id]} not found" }, status: :not_found
-  end
-
   def plant_params
-    params.require(:plant).permit(:name, :image, :price)
+    params.permit(:name, :image, :price)
   end
 end
-
